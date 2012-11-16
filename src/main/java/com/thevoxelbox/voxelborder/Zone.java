821c9e2317dff1647a9ac91ac7939b1b690ff410
TheCryoknight <thecryoknight1@gmail.com>
2012-11-16 12:27:30 -0600

    Fixed rectangle resolution

--- src/main/java/com/thevoxelbox/voxelborder/Zone.java
+++ src/main/java/com/thevoxelbox/voxelborder/Zone.java
@@ -16,31 +16,85 @@ import org.bukkit.block.Block;
 public class Zone implements Serializable {
 	
 	private static final long serialVersionUID = -640268786266506912L;
-	private String zoneName;
-	private UUID worldID;
+	private final String zoneName;
+	private final UUID worldID;
 	private final int lowX;
 	private final int lowZ;
 	private final int highX;
 	private final int highZ;
 
-	public Zone(final String zoneName, final int lowX, final int lowZ, final int highX, final int highZ, UUID worldID) {
+	public Zone(final String zoneName, final int x1, final int z1, final int x2, final int z2, final UUID worldID) {
 		this.worldID = worldID;
 		this.zoneName = zoneName;
-		this.lowX = lowX;
-		this.lowZ = lowZ;
-		this.highX = highX;
-		this.highZ = highZ;
+		if((x2 > x1) && (z2 > z1)) {
+			this.lowX = x1;
+			this.lowZ = z1;
+			this.highX = x2;
+			this.highZ = z2;
+			return;
+		} else if((x2 < x1) && (z2 < z1)) {
+			this.highX = x1;
+			this.highZ = z1;
+			this.lowX = x2;
+			this.lowZ = z2;
+			return;
+		} else if ((x2 < x1) && (z2 > z1)) {
+			this.highX = x1;
+			this.lowZ = z1;
+			this.lowX = x2;
+			this.highZ = z2;
+			return;
+		} else if((x2 > x1) && (z2 < z1)) {
+			this.highX = x1;
+			this.lowZ = z1;
+			this.lowX = x2;
+			this.highZ = z2;
+			return;
+		} else {
+			this.lowX = x1;
+			this.lowZ = z1;
+			this.highX = x2;
+			this.highZ = z2;
+			return;
+		}
 	}
 
 	public Zone(final String zoneName, final Location point1, final Location point2) {
 		this.zoneName = zoneName;
-		this.lowX = point1.getBlockX();
-		this.lowZ = point1.getBlockZ();
-		this.highX = point2.getBlockX();
-		this.highZ = point2.getBlockZ();
 		this.worldID = point1.getWorld().getUID();
+		final int x1 = point1.getBlockX(), z1 = point1.getBlockZ(), x2 = point2.getBlockX(), z2 = point2.getBlockZ();
+		if((x2 > x1) && (z2 > z1)) {
+			this.lowX = x1;
+			this.lowZ = z1;
+			this.highX = x2;
+			this.highZ = z2;
+			return;
+		} else if((x2 < x1) && (z2 < z1)) {
+			this.highX = x1;
+			this.highZ = z1;
+			this.lowX = x2;
+			this.lowZ = z2;
+			return;
+		} else if ((x2 < x1) && (z2 > z1)) {
+			this.highX = x1;
+			this.lowZ = z1;
+			this.lowX = x2;
+			this.highZ = z2;
+			return;
+		} else if((x2 > x1) && (z2 < z1)) {
+			this.highX = x1;
+			this.lowZ = z1;
+			this.lowX = x2;
+			this.highZ = z2;
+			return;
+		} else {
+			this.lowX = x1;
+			this.lowZ = z1;
+			this.highX = x2;
+			this.highZ = z2;
+			return;
+		}
 	}
-
 	public void finishRead() {
 		VoxelBorder.log.info("[VoxelBorder] Border \"" + this.zoneName + "\" has been loaded!");
 	}
